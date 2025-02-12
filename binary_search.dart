b122933967e08c91b2df064fca48a070c8f97c2e
Abdulloh <ganievabdulloh2005@gmail.com>
2025-02-12 09:38:47 +0500

    comment yozildi

---
+++ binary_search.dart
@@ -0,0 +1,11 @@
+void binary_search(List<int> numbers) {
+  int left = 0;
+  int right = numbers.length-1;
+
+  while (left<=right){
+  }
+}
+
+void main(List<String> args) {
+  binary_search([1, 2, 3, 4, 5, 6, 7, 8, 9, 12]);
+}
